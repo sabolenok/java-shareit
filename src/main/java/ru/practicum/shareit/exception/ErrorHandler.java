161bc2066db63ba2fb2888603eb6924d89de43bc
sabolenok <d5sp2004@yandex.ru>
2022-11-24 12:20:00 +0530

    CR: в ErrorHandler добавлена обработка необрабатываемого исключения

--- src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -39,4 +39,10 @@ public class ErrorHandler {
     public ErrorResponse handleAlreadyExistEmail(final AlreadyExistEmailException e) {
         return new ErrorResponse(e.getMessage());
     }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleThrowable(final Throwable e) {
+        return new ErrorResponse("Произошла непредвиденная ошибка.");
+    }
 }
