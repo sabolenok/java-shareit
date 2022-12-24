5e634e6a023b7c4ff259ec59a181ce5b53ce99b2
sabolenok <d5sp2004@yandex.ru>
2022-12-24 15:22:31 +0530

    Остальные тесты для UserController

--- src/test/java/ru/practicum/shareit/UserControllerTests.java
+++ src/test/java/ru/practicum/shareit/UserControllerTests.java
@@ -16,8 +16,11 @@ import ru.practicum.shareit.user.UserService;
 import ru.practicum.shareit.user.dto.UserDto;
 
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -55,7 +58,7 @@ public class UserControllerTests {
     }
 
     @Test
-    void saveNewUserTest() throws Exception {
+    void createNewUserTest() throws Exception {
         when(userService.create(any()))
                 .thenReturn(user);
         mockMvc.perform(post("/users")
@@ -69,4 +72,49 @@ public class UserControllerTests {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.['email']").value(userDto.getEmail()));
     }
 
+    @Test
+    void getAllUserTest() throws Exception {
+        when(userService.findAll())
+                .thenReturn(List.of(user));
+        mockMvc.perform(get("/users"))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$", hasSize(1)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].id").value(userDto.getId()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value(userDto.getName()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].email").value(userDto.getEmail()));
+    }
+
+    @Test
+    void getOneUserTest() throws Exception {
+        when(userService.findById(anyInt()))
+                .thenReturn(user);
+        mockMvc.perform(get("/users/1"))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.['id']").value(userDto.getId()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.['name']").value(userDto.getName()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.['email']").value(userDto.getEmail()));
+    }
+
+    @Test
+    void updateUserTest() throws Exception {
+        user.setEmail("new@test.ru");
+        user.setName("test_user_new");
+        when(userService.put(anyInt(), any()))
+                .thenReturn(user);
+        mockMvc.perform(patch("/users/1")
+                        .content(objectMapper.writeValueAsString(user))
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.['id']").value(userDto.getId()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.['name']").value("test_user_new"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.['email']").value("new@test.ru"));
+    }
+
+    @Test
+    void deleteUserTest() throws Exception {
+        mockMvc.perform(delete("/users/1"))
+                .andExpect(status().isOk());
+    }
 }
